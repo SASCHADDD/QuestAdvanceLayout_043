7ab05138220ae86613fc857e9ae654f14b48c913
SASCHADDD <saschadanu23@gmail.com>
2025-10-24 16:16:13 +0700

    membuat column yang berisikan alamat dan menyetting layout serta mengubah warna font

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -75,7 +75,7 @@ fun ActivitasPertama(modifier: Modifier){
                     Text(
                         stringResource(R.string.alamat),
                         fontSize = 20.sp,
-                        color = Color.White,
+                        color = Color.Yellow,
                         modifier = Modifier.padding(top = 10.dp, bottom = 20.dp)
                     )
                 }
