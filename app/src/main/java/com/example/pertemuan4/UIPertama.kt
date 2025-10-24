1fdca7a1682040d3bca0866b4466b3672e4dacfb
SASCHADDD <saschadanu23@gmail.com>
2025-10-24 15:45:05 +0700

    menambahkan file gambar dan menyettingnya

--- app/src/main/java/com/example/pertemuan4/UIPertama.kt
+++ app/src/main/java/com/example/pertemuan4/UIPertama.kt
@@ -1,11 +1,14 @@
 package com.example.pertemuan4
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -13,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -46,6 +50,16 @@ fun ActivitasPertama(modifier: Modifier){
                 containerColor = Color.LightGray
             )
         ){
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ){
+                val gambar = painterResource(id = R.drawable.logokb)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 150.dp)
+                        .padding(all = 5.dp)
+                )
 
         }
     }
