e2f766fcdecf4596df3ef8ae8549d510aa32f523
SASCHADDD <saschadanu23@gmail.com>
2025-10-25 14:24:41 +0700

    membuat function untuk UIKeduamembuat tampilan foto profile dan menyettingnya menjadi bentuk lingkaran, juga memnambahkan border berupa stroke agar tampilan lebih menarik

--- app/src/main/java/com/example/pertemuan4/UIKedua.kt
+++ app/src/main/java/com/example/pertemuan4/UIKedua.kt
@@ -1,12 +1,21 @@
 package com.example.pertemuan4
 
 import android.provider.ContactsContract
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -14,7 +23,22 @@ fun Profile (modifier: Modifier){
     Column (modifier = Modifier
     .padding(top = 100.dp)
         .fillMaxSize(),
-        horizontalAlignment = Alignment.Start
-    ) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+val foto_profile = painterResource(R.drawable.fotoku)
+        Image(
+            painter = foto_profile,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(180.dp)
+                .clip(CircleShape)
+                .border(
+                    width = 4.dp,
+                    color = Color.LightGray,
+                    shape = CircleShape
+                )
+        )
     }
+}
 
