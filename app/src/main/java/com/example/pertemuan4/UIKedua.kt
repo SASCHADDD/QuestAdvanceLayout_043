06dbb6664b8c7ad135133d708624f9efef086c9f
SASCHADDD <saschadanu23@gmail.com>
2025-10-25 20:29:21 +0700

    membuat composable agar mempermudah untuk pembuatan card , jadi hanya di panggil panggil saat di tampilakan menggunakan resource

--- app/src/main/java/com/example/pertemuan4/UIKedua.kt
+++ app/src/main/java/com/example/pertemuan4/UIKedua.kt
@@ -1,17 +1,9 @@
 package com.example.pertemuan4
 
-import android.text.style.BackgroundColorSpan
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -21,6 +13,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -29,12 +22,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun Profile (modifier: Modifier) {
-
+fun Profile(modifier: Modifier = Modifier) {
     Column(
-        modifier = Modifier
-            .padding(top = 100.dp)
-            .fillMaxSize(),
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 40.dp, start = 1.dp, end = 16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         val fotoprofile = painterResource(R.drawable.fotoku)
@@ -55,12 +47,10 @@ fun Profile (modifier: Modifier) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(4.dp),
             verticalAlignment = Alignment.CenterVertically
-
-        )
-        {
-            val ikon = painterResource(R.drawable.twitter)
+        ) {
+            val ikon1 = painterResource(R.drawable.twitter)
             Image(
-                painter = ikon,
+                painter = ikon1,
                 contentDescription = null,
                 modifier = Modifier.size(26.dp)
             )
@@ -84,46 +74,68 @@ fun Profile (modifier: Modifier) {
             )
         }
         Spacer(modifier = Modifier.height(16.dp))
-
         Text(
-            stringResource(R.string.nama),
+            text = stringResource(R.string.nama),
             fontWeight = FontWeight.SemiBold,
             fontSize = 28.sp,
             color = Color.Black
         )
         Text(
-            stringResource(R.string.username),
+            text = stringResource(R.string.username),
             fontWeight = FontWeight.Thin,
             fontSize = 20.sp,
             color = Color.Black
         )
-        Spacer(modifier = Modifier.height(16.dp))
+        Spacer(modifier = Modifier.height(24.dp))
 
-        Card(
-            modifier = Modifier
-                .height(120.dp)
-                .fillMaxWidth()
-                .padding(12.dp),
-            colors = CardDefaults.cardColors(
-                containerColor = Color.LightGray
+        Column(verticalArrangement = Arrangement.spacedBy(12.dp)) {
+            MenuItem(
+                painterResource(R.drawable.satu),
+                stringResource(R.string.menu1)
             )
-        ) {
-            // isi dari Card
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(
-                    text = "Privacy",
-                    fontSize = 20.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.Black
-                )
-            }
+            MenuItem(
+                painterResource(R.drawable.dua),
+                stringResource(R.string.menu2)
+            )
+            MenuItem(
+                painterResource(R.drawable.tiga),
+                stringResource(R.string.menu3)
+            )
         }
     }
 }
 
+
+@Composable
+fun MenuItem(icon: Painter, text: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        border = BorderStroke(2.dp, Color(0xFF1671B3))
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = icon,
+                contentDescription = null,
+                modifier = Modifier
+                    .size(50.dp)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            Text(
+                text = text,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color(0xFF1671B3)
+            )
+        }
+    }
+}
